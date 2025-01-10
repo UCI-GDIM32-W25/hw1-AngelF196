6ee356574a4d32a8ffbdad96486e4c187b5763ea
AngelF196 <aaflore6@uci.edu>
2025-01-10 12:04:28 -0800

    All editor and programming tasks done, needs devlog

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,20 +10,47 @@ public class Player : MonoBehaviour
     [SerializeField] private PlantCountUI _plantCountUI;
 
     private int _numSeedsLeft;
-    private int _numSeedsPlanted;
+    private int _numSeedsPlanted = 0;
 
     private void Start ()
     {
-        
+        _plantCountUI.UpdateSeeds(_numSeeds, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        if (Input.GetKey("w"))
+        {
+            transform.position += Vector3.up * _speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * _speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += Vector3.down * _speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * _speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (_numSeeds > 0)
+            {
+                PlantSeed();
+            }
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        Debug.Log("trying to plant");
+        _numSeeds -= 1;
+        _numSeedsPlanted += 1;
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+        _plantCountUI.UpdateSeeds(_numSeeds, _numSeedsPlanted);
     }
 }
